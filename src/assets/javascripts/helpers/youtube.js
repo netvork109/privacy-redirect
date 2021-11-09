11e73aceec5a685fad5e6c990b62abd55a8bc00c
netvork109 <netvork109@gmail.com>
2021-11-09 12:53:31 +0500

    Uploaded Invidious instances

--- src/assets/javascripts/helpers/youtube.js
+++ src/assets/javascripts/helpers/youtube.js
@@ -9,36 +9,23 @@ const targets = [
   "s.ytimg.com",
   "music.youtube.com",
 ];
-/*
-    Please remember to also update the manifest.json file
-    (content_scripts > matches, 'persist-invidious-prefs.js')
-    when updating this list:
-  */
+
 const redirects = [
   "https://invidious.snopyta.org",
-  "https://invidious.xyz",
-  "https://invidious.kavin.rocks",
-  "https://tube.connect.cafe",
-  "https://invidious.zapashcanon.fr",
-  "https://invidiou.site",
+  "https://yt.artemislena.eu",
+  "https://yt.didw.to",
+  "https://youtube.076.ne.jp",
+  "https://invidio.xamh.de",
+  "https://invidious.osi.kr",
   "https://vid.mint.lgbt",
-  "https://invidious.site",
-  "https://yewtu.be",
-  "https://invidious.tube",
-  "https://invidious.silkky.cloud",
-  "https://invidious.himiko.cloud",
-  "https://inv.skyn3t.in",
-  "https://tube.incognet.io",
-  "https://invidious.tinfoil-hat.net",
+  "https://inv.riverside.rocks",
   "https://invidious.namazso.eu",
   "https://vid.puffyan.us",
-  "https://dev.viewtube.io",
-  "https://invidious.048596.xyz",
-  "http://fz253lmuao3strwbfbmx46yu7acac2jz27iwtorgmbqlkurlclmancad.onion",
-  "http://qklhadlycap4cnod.onion",
-  "http://c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion",
-  "http://w6ijuptxiku4xpnnaetxvnkc5vqcdu7mgns2u77qefoixi63vbvnpnqd.onion",
-];
+  "https://invidious-jp.kavin.rocks",
+  "https://invidious-us.kavin.rocks",
+  "https://invidious.kavin.rocks",
+  "https://yewtu.be"
+]
 
 export default {
   targets,
